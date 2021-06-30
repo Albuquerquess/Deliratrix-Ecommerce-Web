8bcda14be23dbabac4715ec317f730aea733862a
Albuquerquess <albuquerque.develop@gmail.com>
2021-06-30 12:37:06 -0300

    add global naviation

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch} from 'react-router-dom'
 import Container from './Assets/styles/Common/Container';
+// Components
+import Navigation from './Components/Navigation';
+
+// Pages
 import Home from './Pages/Home';
 
 const Routes: React.FC = () => {
   return <BrowserRouter>
     <BrowserRouter>
+      <Navigation />
       <Switch>
         <Container>
           <Route exact path="/" component={Home}/>
