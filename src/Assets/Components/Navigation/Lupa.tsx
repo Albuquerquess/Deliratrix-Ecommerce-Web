0c7ef1e67b4ff489832d3d88771987bf7635514c
Albuquerquess <albuquerque.develop@gmail.com>
2021-06-30 12:35:59 -0300

    add svg components

---
+++ src/Assets/Components/Navigation/Lupa.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Lupa: React.FC = () => {
+  return <svg id="Lupa" xmlns="http://www.w3.org/2000/svg" width="18.137" height="18.137" viewBox="0 0 18.137 18.137">
+  <g id="search" transform="translate(0 0)">
+    <path id="Caminho_2" data-name="Caminho 2" d="M17.839,16.409l-3.425-3.415a7.978,7.978,0,0,0,1.7-4.936,8.058,8.058,0,1,0-8.058,8.058,7.978,7.978,0,0,0,4.936-1.7l3.415,3.425a1.011,1.011,0,1,0,1.43-1.43ZM2.015,8.058A6.044,6.044,0,1,1,8.058,14.1,6.044,6.044,0,0,1,2.015,8.058Z" transform="translate(0)" fill="#ff4848"/>
+  </g>
+</svg>
+;
+}
+
+export default Lupa;
