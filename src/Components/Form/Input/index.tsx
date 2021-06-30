5eee512e31d5a23cd527567401dd0235dedc34f4
Albuquerquess <albuquerque.develop@gmail.com>
2021-06-30 12:36:57 -0300

    create input component

---
+++ src/Components/Form/Input/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// Styles
+import { LabelInput } from './styles';
+
+// Types
+import { InputProps } from '../../../@types/Form'
+
+const Input: React.FC<InputProps> = ({type}) => {
+  return <LabelInput>
+      <input type={type || 'text'} />
+  </LabelInput>;
+}
+
+export default Input;
---
+++ src/Components/Form/Input/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const LabelInput = styled.label`
+  input {
+      background: var(--color-gray-5);
+      border-radius: .2rem;
+      color: var(--color-white);
+
+      
+
+      font-size: 1.4rem;
+      padding:.5rem;
+  }
+`;
