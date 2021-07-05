faf4f6492b29d4df8422189917c8522ec26657fe
Albuquerquess <albuquerque.develop@gmail.com>
2021-07-05 13:07:54 -0300

    create .App class style and .full-width class

--- src/Assets/styles/globalStyles.ts
+++ src/Assets/styles/globalStyles.ts
@@ -13,20 +13,35 @@ const GlobalStyles = createGlobalStyle`
         --color-white: #FFFFFF;
 
         --max-size-container: 1600px;
+        
 
         background-color: var(--color-black);
         color: var(--color-white);
 
         font-size: 62.5%;
         font-family: 'Ubuntu', sans-serif;
+
+        overflow-x: hidden;
+    }
+
+    body > div#root > div.App {    
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
     }
 
     body {
-        font-size: 1.6rem;
+      font-size: 1.6rem;
 
         display: flex;
         align-items: center;
         justify-content: center;
+
+        div.full-width {
+            width: 100%
+        }
+
     }
 `
 
