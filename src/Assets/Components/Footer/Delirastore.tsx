1ad7d8bf36a92e22ee72d7a5f2a4c392be145479
Albuquerquess <albuquerque.develop@gmail.com>
2021-07-06 16:22:07 -0300

    Rewrite  svg props

--- src/Assets/Components/Footer/Delirastore.tsx
+++ src/Assets/Components/Footer/Delirastore.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const DeliraStore: React.FC = () => {
   return <svg xmlns="http://www.w3.org/2000/svg" width="256" height="34" viewBox="0 0 256 34">
-  <text id="Delirastore" transform="translate(0 28)" fill="#ff4848" font-size="30" font-family="Ubuntu-Regular, Ubuntu" letter-spacing="0.207em">
+  <text id="Delirastore" transform="translate(0 28)" fill="#ff4848" fontSize="30" fontFamily="Ubuntu-Regular, Ubuntu" letterSpacing="0.207em">
     <tspan x="0" y="0">DELIRA</tspan><tspan y="0" fill="#fff">STORE</tspan>
   </text>
 </svg>
