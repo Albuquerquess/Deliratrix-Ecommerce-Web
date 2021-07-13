f49aa216200ce04b15f795ee0db372324d25da4f
Albuquerquess <albuquerque.develop@gmail.com>
2021-07-13 09:35:47 -0300

    CardButton redirect to page

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -33,7 +33,7 @@ const Home: React.FC = () => {
   return <>
     <SearchInput onlyMobile={true} value={search} setValue={setSearch} redirectTo={'/search'}/>
     <Header />
-    <Box title="Produtos mais comprados">
+    <Box title="Serviços mais contratados">
       <Card
         title="ASMR"
         description="ASMR é uma sigla que define gatilhos sensoriais que relaxam profundamente, causando arrepios, sono e sensação de formigamento. Se você deseja uma... experiência sedante e repleta de prazer, este é o serviço ideal!"
@@ -81,7 +81,7 @@ const Home: React.FC = () => {
         category="orgia"
         id={1}
         prices={prices} />
-      <CardButton label="Ver todos os produtos"/>
+      <CardButton label="Ver todos os Serviços" to="/explorar/Servicos"/>
     </Box>
     
     <Box title="Produtos mais comprados" >
@@ -109,7 +109,7 @@ const Home: React.FC = () => {
         image="https://images.unsplash.com/photo-1625093440233-1dac60534a68?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80"
         value="R$ 5/min"
         rate={4} />
-      <CardButton label="Ver todos os produtos"/>
+      <CardButton label="Ver todos os produtos" to="/explorar/Produtos"/>
     </Box>
     
  </>
