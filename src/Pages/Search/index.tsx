f240530c7bc3e281d34d99c6f5053a8ee4bc08c2
Albuquerquess <albuquerque.develop@gmail.com>
2021-08-10 14:26:49 -0300

    reduces search time. Add id to Card

--- src/Pages/Search/index.tsx
+++ src/Pages/Search/index.tsx
@@ -37,10 +37,9 @@ const Search: React.FC = () => {
   const [selectValue, setSelectValue] = React.useState('rate')
   const [modalIsOpen, setModalIsOpen] = React.useState(false)
   const [search, setSearch] = React.useState('')
-  const searchDebounce = useDebounce(search, 1000)
+  const searchDebounce = useDebounce(search, 500)
   const [searchData, setSearchData] = React.useState<searchDataProps[]>([])
 
-  
   async function getContentOnbackendByFilter() {
     const response = await Api.get(INDEX_SEARCH, {params: {search: searchDebounce, orderBy: selectValue}})
     setSearchData(response.data)
@@ -112,7 +111,7 @@ const Search: React.FC = () => {
           rate={content.rate}
           type={content.type}
           category={content.category}
-          id={1} />) : <Loading />}
+          id={content.id} />) : <Loading />}
       </Box>
   </SearchContainer>;
 }
